d574603302973b935ec2fd0b005613895b6d23d4
ibit <60405790+ibit@users.noreply.github.com>
2025-08-14 21:33:38 +0900

    Add files via upload

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,5 +1,5 @@
 number = float(input("数字を入力してください："))
-operator = input("演算記号を入力してください：")
+operator = input("演算子を入力してください：")
 next_number = float(input("次の数字を入力してください："))
 
 result = number
@@ -12,5 +12,7 @@ elif operator == "*":
     result *= next_number
 elif operator == "/":
     result /= next_number
+else:
+    print("無効な演算子です。")
 
 print("計算結果：", result)
