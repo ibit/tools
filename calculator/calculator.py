5defbd57b662060cab4911a565e8b5e3f06a26ca
ibit <60405790+ibit@users.noreply.github.com>
2025-08-16 15:49:47 +0900

    修正済み Close #1

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,19 +1,30 @@
+#numberに値を入力
 number = float(input("数字を入力してください："))
-operator = input("演算子を入力してください：")
-next_number = float(input("次の数字を入力してください："))
 
-result = number
+#operatorに演算子を入力
+while True:
+    operator = input("演算子を入力してください(計算を終了する場合はeを入力)：")
 
-if operator == "+":
-    result += next_number
-elif operator == "-":
-    result -= next_number
-elif operator == "*":
-    result *= next_number
-elif operator == "/":
-    result /= next_number
-else:
-    print("無効な演算子です。")
-    exit()
+    if operator == "e":
+        break
+        
+    try:
+        next_number = float(input("次の数字を入力してください："))
+    except ValueError:
+        print("数字を入力してください。")
+        continue
 
-print("計算結果：", result)
+    if operator == "+":
+        number += next_number
+    elif operator == "-":
+        number -= next_number
+    elif operator == "*":
+        number *= next_number
+    elif operator == "/":
+        number /= next_number
+    else:
+        print("無効な演算子です。")
+        continue
+
+    print("計算結果：", number)
+print("最終結果：", number)
