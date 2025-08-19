61be8bfcd7465a943ad806cd1b1f67f39f6a56b6
ibit <60405790+ibit@users.noreply.github.com>
2025-08-19 16:43:44 +0900

    Add files via upload

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,33 +1,33 @@
-#numberに値を入力
-number = float(input("数字を入力してください："))
-
-#operatorに演算子を入力
 while True:
-    operator = input("演算子を入力してください(計算を終了する場合はeを入力)：").lower()
+    number = float(input("数字を入力してください："))
 
-    if operator.lower() == "e":
-        break
-        
-    try:
-        next_number = float(input("次の数字を入力してください："))
-    except ValueError:
-        print("数字を入力してください。")
-        continue
+    while True:
+        operator = input("演算子を入力してください(計算を終了する場合はeを入力)：").lower()
 
-    if operator == "+":
-        number += next_number
-    elif operator == "-":
-        number -= next_number
-    elif operator == "*":
-        number *= next_number
-    elif operator == "/":
-        if next_number == 0:
-            print("0で割ることはできません。")
+        if operator == "e":
+            break
+
+        try:
+            next_number = float(input("次の数字を入力してください："))
+        except ValueError:
+            print("数字を入力してください。")
             continue
-        number /= next_number
-    else:
-        print("無効な演算子です。")
-        continue
 
-    print("途中結果：", number)
-print("最終結果：", number)
+        if operator == "+":
+            number += next_number
+        elif operator == "-":
+            number -= next_number
+        elif operator == "*":
+            number *= next_number
+        elif operator == "/":
+            if next_number == 0:
+                print("0で割ることはできません。")
+                continue
+            number /= next_number
+        else:
+            print("無効な演算子です。")
+            continue
+
+        print("途中結果：", number)
+
+    print("最終結果：", number)
